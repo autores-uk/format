97a7b371a7b47dd3b24d78d95afff23c0f212b7d
autores-uk <autores.uk@protonmail.me>
2024-05-10 11:29:55 +0100

    Documentation

---
+++ code/format/src/main/java/uk/autores/format/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>AutoRes Format</h1>
+ *
+ * <p>
+ *     An alternative to {@link java.text.MessageFormat}.
+ * </p>
+ * <p>
+ *     Use
+ *     {@link uk.autores.format.Formatting#parse(java.lang.CharSequence)}
+ *     to parse expressions.
+ *     Use
+ *     {@link uk.autores.format.Formatting#format(java.util.List, java.util.Locale, java.lang.Object...)}
+ *     to format strings.
+ * </p>
+ * <pre><code>
+ *     // parse to immutable expression
+ *     List&lt;FormatSegment&gt; expression = Formatting.parse("Hello, {0}!");
+ *     // format
+ *     Locale l = Locale.getDefault();
+ *     String result = Formatting.format(expression, l, "World");
+ * </code></pre>
+ */
+package uk.autores.format;
