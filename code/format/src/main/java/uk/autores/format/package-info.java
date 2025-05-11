c14fe229bcd5967388f5aab87a647872a62e87bb
autores-uk <autores.uk@protonmail.me>
2025-05-11 18:50:50 +0100

    Fixed Javadoc

--- code/format/src/main/java/uk/autores/format/package-info.java
+++ code/format/src/main/java/uk/autores/format/package-info.java
@@ -27,11 +27,11 @@
  *     All
  *     <a href="https://docs.oracle.com/en/java/javase/23/docs/api/java.base/java/text/MessageFormat.html" target="_blank">JDK23
  *     expressions</a> are supported under JDK8+ runtimes with the following exceptions.
- *     <ul>
- *         <li>Formatting compact "number" expressions like <code>{0,number,compact_short}</code> requires a JDK12+ runtime</li>
- *         <li>Formatting "list" expressions like <code>{0,list}</code> requires a JDK22+ runtime</li>
- *     </ul>
  * </p>
+ * <ul>
+ *     <li>Formatting compact "number" expressions like <code>{0,number,compact_short}</code> requires a JDK12+ runtime</li>
+ *     <li>Formatting "list" expressions like <code>{0,list}</code> requires a JDK22+ runtime</li>
+ * </ul>
  * <p>
  *     See <a href="https://github.com/autores-uk/format" target="_blank">GitHub</a> for more information.
  * </p>
