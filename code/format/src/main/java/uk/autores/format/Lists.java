55224b0975aab2e9fd84beb6bc7ef35ec54360c4
autores-uk <autores.uk@protonmail.me>
2025-07-23 15:24:37 +0100

    Minor enhancement

--- code/format/src/main/java/uk/autores/format/Lists.java
+++ code/format/src/main/java/uk/autores/format/Lists.java
@@ -11,52 +11,48 @@ import java.util.Locale;
  * Compile target is less than JDK22 so uses reflection.
  */
 final class Lists {
-    private static final Object MUTEX = new Object();
-    private static volatile Method getInstance;
-    private static volatile Method format;
-    private static volatile Object STANDARD;
-    private static volatile Object OR;
-    private static volatile Object UNIT;
-    private static volatile Object FULL;
+    private static volatile LF cached;
 
     private Lists() {}
 
     static void format(Locale l, FormatVariable variable, StringBuffer buf, Object... args) {
-        tryInit();
-        Object type = style(variable.style());
-        Object formatter = Reflection.invoke(null, getInstance, l, type, FULL);
-        Object list = args[variable.index()];
-        Reflection.invoke(formatter, format, list, buf, new FieldPosition(0));
-    }
-
-    private static void tryInit() {
-        if (getInstance == null) {
-            synchronized (MUTEX) {
-                if (getInstance == null) {
-                    init();
-                }
-            }
+        if (cached == null) {
+            cached = init();
         }
+        LF ListFormat = cached;
+        Object type = style(ListFormat, variable.style());
+        Object formatter = Reflection.invoke(null, ListFormat.getInstance, l, type, ListFormat.FULL);
+        Object list = args[variable.index()];
+        Reflection.invoke(formatter, ListFormat.format, list, buf, new FieldPosition(0));
     }
 
-    private static void init() {
+    private static LF init() {
         String msg = "Use of java.text.ListFormat requires JDK22 or above";
         Class<?> ListFormat = Reflection.type("java.text.ListFormat", msg);
         Class<?> ListFormatType = Reflection.type("java.text.ListFormat$Type", msg);
         Class<?> ListFormatStyle = Reflection.type("java.text.ListFormat$Style", msg);
-        getInstance = Reflection.meth(ListFormat, "getInstance", Locale.class, ListFormatType, ListFormatStyle);
-        format = Reflection.meth(ListFormat, "format", Object.class, StringBuffer.class, FieldPosition.class);
-        STANDARD = Reflection.field(ListFormatType, "STANDARD");
-        OR = Reflection.field(ListFormatType, "OR");
-        UNIT = Reflection.field(ListFormatType, "UNIT");
-        FULL = Reflection.field(ListFormatStyle, "FULL");
+        Method getInstance = Reflection.meth(ListFormat, "getInstance", Locale.class, ListFormatType, ListFormatStyle);
+        Method format = Reflection.meth(ListFormat, "format", Object.class, StringBuffer.class, FieldPosition.class);
+        Object STANDARD = Reflection.field(ListFormatType, "STANDARD");
+        Object OR = Reflection.field(ListFormatType, "OR");
+        Object UNIT = Reflection.field(ListFormatType, "UNIT");
+        Object FULL = Reflection.field(ListFormatStyle, "FULL");
+
+        return new LF(getInstance, format, STANDARD, OR, UNIT, FULL);
     }
 
-    private static Object style(FmtStyle style) {
+    private static Object style(LF ListFormat, FmtStyle style) {
         return switch (style) {
-            case OR -> OR;
-            case UNIT -> UNIT;
-            default -> STANDARD;
+            case OR -> ListFormat.OR;
+            case UNIT -> ListFormat.UNIT;
+            default -> ListFormat.STANDARD;
         };
     }
+
+    private record LF(Method getInstance,
+                      Method format,
+                      Object STANDARD,
+                      Object OR,
+                      Object UNIT,
+                      Object FULL) {}
 }
